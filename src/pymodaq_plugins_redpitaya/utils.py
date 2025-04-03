cd66dc253beb05adab92b7c9c98c41b7f9e6c3ae
manvrard <smanzuetaf@gmail.com>
2025-04-03 15:42:02 +0200

    updat packaging layout pyproject

--- src/pymodaq_plugins_redpitaya/utils.py
+++ src/pymodaq_plugins_redpitaya/utils.py
@@ -6,10 +6,11 @@ Created the 31/08/2023
 """
 from pathlib import Path
 
-from pymodaq.utils.config import BaseConfig, USER
+from pymodaq_utils.config import BaseConfig, USER
 
 
 class Config(BaseConfig):
     """Main class to deal with configuration values for this plugin"""
     config_template_path = Path(__file__).parent.joinpath('resources/config_template.toml')
     config_name = f"config_{__package__.split('pymodaq_plugins_')[1]}"
+
